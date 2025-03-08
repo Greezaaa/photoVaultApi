e22723e0746113caadad8875098727cd9c9d5831
Greezaaa <greezaaa@gmail.com>
2025-03-08 05:23:43 +0100

    feat: enhance HTTPS configuration and set global API prefix from environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: readFileSync(resolve(__dirname, 'path/to/private.key')),
-  //   cert: readFileSync(resolve(__dirname, 'path/to/certificate.crt')),
-  // };
-  const app = await NestFactory.create(
-    AppModule,
-    // { httpsOptions }
-  );
+  // OpenSSL
+  const httpsOptions = {
+    key: readFileSync(process.env.SSL_KEY ?? '', 'utf8'),
+    cert: readFileSync(process.env.SSL_CERTIFICATE ?? '', 'utf8'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require('tsconfig-paths/register');
@@ -25,9 +24,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  app.setGlobalPrefix('api/v1');
-  console.log(process.env.PORT ?? 3000);
-  await app.listen(process.env.PORT ?? 3000);
+  // Set global API prefix from environment variable
+  const apiPrefix = process.env.API_PREFIX || 'api/v1';
+  app.setGlobalPrefix(apiPrefix);
+
+  // Start the app on a specific host and port
+  const host = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 3000;
+  await app.listen(port, host);
+  console.log(`App running on: https://${host}:${port}/${apiPrefix}`);
 }
 bootstrap().catch((err) => {
   console.error('Error starting the app:', err);

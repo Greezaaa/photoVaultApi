349efc7dc9313b0963abcaac18faa95bb0e5f18e
Greezaaa <greezaaa@gmail.com>
2025-03-09 03:15:28 +0100

    feat: enhance logging in auth module and clean up unused code

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,8 +1,6 @@
-import { Public, UseRoles } from '@decorators';
-import { JwtAuthGuard, RolesGuard, StatusGuard } from '@guards';
-import { UserRoles } from '@interfaces';
-import { Body, Controller, Logger, Post, Request, UseGuards } from '@nestjs/common';
-import { seconds, SkipThrottle, Throttle } from '@nestjs/throttler';
+import { Public } from '@decorators';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { seconds, Throttle } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -14,22 +12,24 @@ export class AuthController {
   @Throttle({ default: { limit: 5, ttl: seconds(10) } })
   @Public()
   async login(@Body() body) {
-    this.logger.log('Login endpoint accessed');
+    this.logger.log(`Login intent by: ${body.email}`);
     const user = await this.authService.validateUser(body.email as string, body.password as string);
 
     if (user) {
       return this.authService.login(user);
     }
+    this.logger.log(`User login failed: with email ${body.email}`);
     return { message: 'Invalid credentials' };
   }
 
-  @SkipThrottle()
-  @Post('protected')
-  @UseGuards(JwtAuthGuard, RolesGuard, StatusGuard)
-  @UseRoles(UserRoles.ADMIN)
+  // TODO: REMOVE BEFORE UPLOAD!!!
+  // @SkipThrottle()
+  // @Post('protected')
+  // @UseGuards(JwtAuthGuard, RolesGuard, StatusGuard)
+  // @UseRoles(UserRoles.ADMIN)
   // @UseStatus(UserStatus.ACTIVE)
-  protected(@Request() req) {
-    console.log(req.user.role);
-    return { message: 'Protected data', user: req.user };
-  }
+  // protected(@Request() req) {
+  //   console.log(req.user.role);
+  //   return { message: 'Protected data', user: req.user };
+  // }
 }
--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@resources/user/entities/user.entity';
 import { UserService } from '@resources/user/user.service';
@@ -6,6 +6,7 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private jwtService: JwtService,
     private userService: UserService,
@@ -16,10 +17,12 @@ export class AuthService {
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
+      this.logger.log(`UnauthorizedException: Invalid credentials ${email}`);
       throw new UnauthorizedException('Invalid credentials');
     }
 
     const { id, name, email: userEmail, role, status } = user;
+    this.logger.log(`User validated successfully: with email ${email}`);
     return { id, name, email: userEmail, role, status };
   }
 
@@ -36,6 +39,7 @@ export class AuthService {
       expiresIn: process.env.JWT_EXPIRES_IN,
     });
 
+    this.logger.log(`User logged in successfully: with email ${user.email}`);
     return { token };
   }
 }
